94ed1682fc031b65d93950d329f7ec43a08b37de
shubhamshri25 <105149595+shubhamshri25@users.noreply.github.com>
2023-03-14 12:48:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Your Script here.
+function rot13(str) {
+  let decoded = '';
+  for (let i = 0; i < str.length; i++) {
+    const charCode = str.charCodeAt(i);
+    if (charCode >= 65 && charCode <= 90) { // uppercase letters only
+      const decodedCharCode = ((charCode - 65 + 13) % 26) + 65;
+      decoded += String.fromCharCode(decodedCharCode);
+    } else {
+      decoded += str[i];
+    }
+  }
+  return decoded;
+}
